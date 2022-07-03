79d7cb00a78e604102b817fcbae03d3d2920e6ba
Magni <mv.magni@gmail.com>
2022-07-03 16:16:17 -0400

    Update
    Mid day update - power failures so why not

--- utilities/DataUtils.py
+++ utilities/DataUtils.py
@@ -9,27 +9,39 @@ def exploreDataframe(data, numRecords=5):
     print("\nLast " + str(numRecords) + " in dataframe")
     display(data.tail(numRecords))
 
+    print(f'')
+    print(f'Null value count by column:')
+    display(data.isna().sum())
+
+
 def showUniqueColVals(dataFrame,
                       colName,
                       showRecords=5):
     dataTypeObj = dataFrame.dtypes[colName]
+    numRecords = len(dataFrame)
+    uniqueItems = dataFrame[colName].unique()
+    numUnique = len(uniqueItems)
+    nullValues = dataFrame[colName].isna().sum()
+    percUnique = str(round(numUnique / numRecords, 2) * 100 ) + "%"
+
     print(f'Data type of column [{colName}] is: {dataTypeObj}')
-    print(f'Total number of rows: {len(dataFrame)}')
-    a = dataFrame[colName].unique()
-    print(f'Unique values: {len(a)}')
+    print(f'Total number of rows: {numRecords}')
+
+    print(f'Unique values in column: {numUnique} [percent unique: {percUnique}]')
+    print(f'Null values in column: {nullValues}')
     print(f'List of unique values:')
-    # print(sort(a)) # not all series are sortable apparently
-    print(a)
+    # print(sort(uniqueItems)) # not all series are sortable apparently
+    print(uniqueItems)
 
     sumDF = dataFrame.groupby([colName]).size().to_frame('record_count')
     sumDF.reset_index(inplace=True)
 
     print(f'')
-    print(f'Showing top {showRecords} records for {colName} by frequency')
+    print(f'Top {showRecords} records by frequency for {colName}')
     sumDF = sumDF.sort_values(by=['record_count'], ascending=False)
     print(sumDF.head(showRecords))
 
     print(f'')
-    print(f'Showing bottom {showRecords} records for {colName} by frequency')
+    print(f'Bottom {showRecords} records by frequency for {colName}')
     sumDF = sumDF.sort_values(by=['record_count'], ascending=True)
     print(sumDF.head(showRecords))
