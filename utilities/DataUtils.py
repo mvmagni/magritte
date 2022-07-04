5cc866eec7a9e911dcbc7ae2071b4f078d09171f
Magni <mv.magni@gmail.com>
2022-07-03 22:44:55 -0400

    Nightly commit
    nightly

--- utilities/DataUtils.py
+++ utilities/DataUtils.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def exploreDataframe(data, numRecords=5):
+def exploreDataframe(data, showRecords=5):
     print("dataframe shape: " + str(data.shape))
     print("\ndataframe info: ")
     print(data.info())
-    print("\nFirst " + str(numRecords) + " in dataframe")
-    display(data.head(numRecords))
-    print("\nLast " + str(numRecords) + " in dataframe")
-    display(data.tail(numRecords))
+    print("\nFirst " + str(showRecords) + " in dataframe")
+    display(data.head(showRecords))
+    print("\nLast " + str(showRecords) + " in dataframe")
+    display(data.tail(showRecords))
 
     print(f'')
     print(f'Null value count by column:')
@@ -22,7 +22,7 @@ def showUniqueColVals(dataFrame,
     uniqueItems = dataFrame[colName].unique()
     numUnique = len(uniqueItems)
     nullValues = dataFrame[colName].isna().sum()
-    percUnique = str(round(numUnique / numRecords, 2) * 100 ) + "%"
+    percUnique = str(round(numUnique / numRecords, 3) * 100 ) + "%"
 
     print(f'Data type of column [{colName}] is: {dataTypeObj}')
     print(f'Total number of rows: {numRecords}')
@@ -40,8 +40,38 @@ def showUniqueColVals(dataFrame,
     print(f'Top {showRecords} records by frequency for {colName}')
     sumDF = sumDF.sort_values(by=['record_count'], ascending=False)
     print(sumDF.head(showRecords))
+    topList = sumDF[colName].head(showRecords).tolist()
+
+
 
     print(f'')
     print(f'Bottom {showRecords} records by frequency for {colName}')
     sumDF = sumDF.sort_values(by=['record_count'], ascending=True)
     print(sumDF.head(showRecords))
+    bottomList = sumDF[colName].head(showRecords).tolist()
+
+    return topList, bottomList
+
+
+def dropNullRows(dataFrame, columns=None, showRecords=1):
+    if columns is None:
+        print(f'Dropping rows where any column is null')
+    else:
+        print(f'Dropping rows where {columns} is/are null')
+
+    print(f'')
+    print(f'Original dataFrame shape: {dataFrame.shape}')
+    print(f'Original null value count by column:')
+    display(dataFrame.isna().sum())
+    if columns is None or len(columns) == 0:
+        tDF = dataFrame.dropna(axis=0, subset=None)
+    else: # Specified some sort of column
+        tDF = dataFrame.dropna(axis=0, subset=None)
+
+    print(f'')
+    print(f'*** Rows with nulls meeting criteria have been dropped')
+    print(f'')
+    print(f'New values:')
+    exploreDataframe(tDF, showRecords=1)
+    print(f'New dataframe returned')
+    return tDF
